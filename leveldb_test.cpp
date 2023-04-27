0a1d6b465fc8c93b8c28dba4d6f35ccb0a0549ec
Nixianjun6 <11790399+nixianjun6@user.noreply.gitee.com>
2023-04-27 12:06:48 +0800

    update reads and writes result

--- leveldb_test.cpp
+++ leveldb_test.cpp
@@ -65,16 +65,19 @@ int main() {
   GetData(mydb, "zhaokangkang", result);
   std::cout << "zhaokangkang : " << result << std::endl;
 
-  //nixianjun:
-  //a handsome man zhaokangkang : a beautiful girl
-  //delete nixianjun
-  //nixianjun : a beautiful girl
-  //zhaokangkang : a beautiful girl
-  //delete zhaokangkang 
-  //nixianjun : a beautiful girl
-  //zhaokangkang : a beautiful girl
-  //put xxx
-  //nixianjun : a beautiful girl zhaokangkang : a beautiful girl
+  /*
+  nixianjun:a handsome man
+  zhaokangkang : a beautiful girl
+  delete nixianjun
+  nixianjun : a beautiful girl
+  zhaokangkang : a beautiful girl
+  delete zhaokangkang 
+  nixianjun : a beautiful girl
+  zhaokangkang : a beautiful girl
+  put xxx
+  nixianjun : a beautiful girl
+  zhaokangkang : a beautiful girl
+  */
 
   std::cout << "------------------------------" << std::endl;
   
@@ -88,6 +91,11 @@ int main() {
   GetData(mydb, "nixianjun", result);
   std::cout << "nixianjun : " << result << std::endl;
 
+  // 测试原子批次更新
+
+
+  // 删除数据库
+  mystatus = leveldb::DestroyDB("testdb", myoptions);
   delete mydb;
 
 }
